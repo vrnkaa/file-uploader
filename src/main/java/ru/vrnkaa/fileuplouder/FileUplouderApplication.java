a7990e34c4544fcdb04d59983c61f07487155c93
alex <alex@vrnkaa.com>
2022-02-16 10:24:17 +0300

    initial

---
+++ src/main/java/ru/vrnkaa/fileuplouder/FileUplouderApplication.java
@@ -0,0 +1,13 @@
+package ru.vrnkaa.fileuplouder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUplouderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUplouderApplication.class, args);
+	}
+
+}
